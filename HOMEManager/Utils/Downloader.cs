f157edac3766774f35b6b7f9c5b5131827fa49ea
Razmoth <32140579+Razmoth@users.noreply.github.com>
2023-02-02 17:47:56 +0400

    Add project files.

---
+++ HOMEManager/Utils/Downloader.cs
@@ -0,0 +1,31 @@
+﻿namespace HOMEManager
+{
+    public static class DLLManager
+    {
+        private const string API = "https://resource.pokemon-home.com";
+        
+        private readonly static Uri Api = new(API);
+        private readonly static HttpClient client;
+        static DLLManager()
+        {
+            client = new HttpClient
+            {
+                BaseAddress = Api
+            };
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (compatible; MSIE 6.0; Windows 98; Trident/5.1)");
+        }
+
+        public static async Task<Stream> DownloadFile(string path)
+        {
+            MemoryStream data = new();
+            if (Uri.TryCreate(Api, path, out var uri))
+            {
+                Console.WriteLine($"Downloading {Path.GetFileName(uri.AbsolutePath)}...");
+                var temp = await client.GetStreamAsync(uri);
+                temp.CopyTo(data);
+                data.Position = 0;
+            }
+            return data;
+        }
+    }
+}
---
+++ HOMEManager/Utils/Utils.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+namespace HOMEManager
+{
+    public static class Utils
+    {
+        private const int MDVR = 50900;
+        private const string CDN = "ph.prd.cdnfiles";
+        private const string MD = "md/dro/diff/MD_AssetbundleDLPackVer.snd";
+        private readonly static string OUTPUT = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bundles");
+
+        static Utils()
+        {
+            Directory.CreateDirectory(OUTPUT);
+        }
+
+        public static string GetMDPath => $"{CalculateFileHash(MDVR)}/{MD}";
+        public static string GetBundlePath(int id, string name) => $"{CalculateFileHash(id)}/dro/{name}.abap";
+        public static string GetBundleLocalPath(string name) => Path.Combine(OUTPUT, $"{name}.abap");
+        public static string GetFilePath(string bundle, string name)
+        {
+            var dir = Path.Combine(OUTPUT, bundle);
+            Directory.CreateDirectory(dir);
+            return Path.Combine(dir, $"{name}.unity3d");
+        }
+        public static void WriteToJSON(object obj, string path)
+        {
+            var str = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            File.WriteAllText(path, str);
+        }
+        public static string CalculateFileHash(int id)
+        {
+            var hash = MD5Hash($"{CDN}/{id}");
+            var idx = Convert.ToByte(hash[..1], 16);
+            hash = string.Concat(hash[idx..], hash[..idx]);
+            return MD5Hash(hash);
+        }
+        private static string MD5Hash(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+            bytes = MD5.HashData(bytes);
+            return Convert.ToHexString(bytes).ToLower();
+        }
+    }
+}
