8b4c5529875c1849f8c689b4854ae0a014f924ff
Razmoth <32140579+Razmoth@users.noreply.github.com>
2023-02-03 00:06:51 +0400

    fixed download issue.

--- HOMEManager/Program.cs
+++ HOMEManager/Program.cs
@@ -15,14 +15,7 @@ if (args.Length == 1)
             stream = File.OpenRead(args[0]);
         }
         var items = MDParser<JsonAssetBundleItem>.Read(stream);
-        var tasks = new List<Task>();
         foreach (var item in items)
-        {
-            tasks.Add(Task.Run(() => Process(item)));
-        }
-        Task.WhenAll(tasks).Wait();
-
-        static async void Process(JsonAssetBundleItem item)
         {
             using var data = await DLLManager.DownloadFile(item.Path);
             ABAPParser.Parse(data, item.nm);
